8bba63fb533f736db89d60bd128f2b4a57b40a15
Eng .Mohamed Abdo <eng.mohamedabdo139@gmail.com>
2023-10-02 20:28:57 +0300

    firest

--- lib/main.dart
+++ lib/main.dart
@@ -31,8 +31,15 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(),
     );
   }
 }
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
 
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
