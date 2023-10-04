ad84c4b9e2514a3bef0cbcd248f65c99d50d76b2
Eng .Mohamed Abdo <eng.mohamedabdo139@gmail.com>
2023-10-04 18:03:34 +0300

    det app colors and assets

---
+++ lib/core/utils/app_assets.dart
@@ -0,0 +1,13 @@
+abstract class Assets {
+  Assets._();
+
+  static const String emailVerImg = "assets/images/emailverimg.png";
+
+  static const String forgetPassImg = "assets/images/forgetpassimg.png";
+
+  static const String logo1 = "assets/images/logo1.png";
+
+  static const String resetPassImg = "assets/images/ressetpassimg.png";
+
+  static const String welcome1 = "assets/images/welcome1.png";
+}
---
+++ lib/core/utils/app_colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+abstract class AppColors{
+  static Color mainColor=const Color(0xffED5B2C);
+  static Color black=const Color(0x00000000);
+  static Color grey=const Color(0xff696969);
+  static Color lightGrey=const Color(0xff8A8A8A);
+}
